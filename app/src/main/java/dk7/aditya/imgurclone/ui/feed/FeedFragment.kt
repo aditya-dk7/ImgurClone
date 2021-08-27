47a2b7b68c381ce5c61b5ba178ceb592b558dcab
Aditya <rameshaditya2001@gmail.com>
2021-08-28 01:27:25 +0530

    Some Ui Cleanup and setting up

---
+++ app/src/main/java/dk7/aditya/imgurclone/ui/feed/FeedFragment.kt
@@ -0,0 +1,38 @@
+package dk7.aditya.imgurclone.ui.feed
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import dk7.aditya.imgurclone.R
+
+class FeedFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = FeedFragment()
+    }
+
+    private lateinit var viewModel: FeedViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val feed = arguments?.getString("feed")
+        val rootView = inflater.inflate(R.layout.feed_fragment, container, false)
+        feed?.let {
+            rootView.findViewById<TextView>(R.id.tvTextView).text = it
+        }
+        return rootView
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(FeedViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/dk7/aditya/imgurclone/ui/feed/FeedViewModel.kt
@@ -0,0 +1,7 @@
+package dk7.aditya.imgurclone.ui.feed
+
+import androidx.lifecycle.ViewModel
+
+class FeedViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
